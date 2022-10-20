c3073da17ac8e21ca844db78782ed1676f069168
Robertson Morales <moralesso152842@gmail.com>
2022-10-20 20:12:05 +0800

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,75 @@
+feather.replace();
+
+// window.scrollTo(0,0);
+
+function navSelected(id){
+    let navbar = document.getElementsByClassName('navbar');
+    for (let i = 0; i < navbar.length; i++) {
+        if (navbar[i].classList.contains('active')) {
+            navbar[i].classList.remove('active');   
+        }
+    }
+
+    let el = document.getElementById(id);
+    if (id == 'hero') {
+        document.getElementById('nav-hero').classList.add('active');
+        window.scrollTo(0,0);
+    }else{
+        document.getElementById('nav-'+id).classList.add('active');
+        el.scrollIntoView();
+    }
+}
+
+function visibleToViewport(el){
+    var rect = el.getBoundingClientRect();
+
+    // return rect;
+    return (
+        rect.top == 0 &&
+        rect.left == 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// const navs = ['hero', 'services', 'works', 'skills', 'about', 'contact'];
+
+// var message = {};
+
+// document.addEventListener('scroll', () => {
+//     message = {
+//         visibiliy: visibleToViewport(document.getElementById('works')) 
+//             ? 'Visible to viewport'
+//             : 'Not Visible to viewport',
+//         rect: document.getElementById('services').getBoundingClientRect()
+//     }
+
+//     // console.log(message);
+// });
+
+$(() => {
+    'use strict';
+
+    var date = new Date();
+    $('#year').html(date.getFullYear());
+
+    $(document).on('scroll', function () {
+        // $("#navbar").toggleClass('border-bottom', $(this).scrollTop() > $("#navbar").height());
+        // $("#navbar").toggleClass('bg-color', $(this).scrollTop() > $("#navbar").height());
+	});
+
+    function navBarHeight(){
+        var set_height = $('#navbar').height() * 2;
+        $('#about').css('margin-top', set_height + "px");
+        $('#services').css('margin-top', set_height + "px");
+        $('#projects').css('margin-top', set_height + "px");
+        $('#skills').css('margin-top', set_height + "px");
+        $('#contact').css('margin-top', set_height + "px");
+    }   
+
+    // navBarHeight(); 
+
+    $('#btn-call').click(function(){
+        window.location.href = "tel:639552808438";
+    });
+});
---
+++ js/scroll.js
@@ -0,0 +1,174 @@
+function useScrolling(el, params){
+    ScrollReveal().reveal(el, params);
+}
+
+useScrolling('nav', {
+    delay: 100,
+    distance: '5%',
+    origin: 'top',
+    easing: 'ease',
+    mobile: false,
+    // interval: 50
+});
+
+useScrolling('#my-profile', {
+    delay: 300,
+    distance: '5%',
+    origin: 'top',
+    easing: 'ease'
+});
+
+useScrolling('#intro', {
+    delay: 100,
+    distance: '5%',
+    origin: 'right',
+    easing: 'ease'
+});
+
+useScrolling('#specialize', {
+    delay: 200,
+    distance: '5%',
+    origin: 'left',
+    easing: 'ease'
+});
+
+// CAll TO ACTIONS
+useScrolling('#btn-my-works', {
+    delay: 300,
+    distance: '15%',
+    origin: 'left',
+    easing: 'ease'
+});
+
+useScrolling('#btn-download-resume', {
+    delay: 300,
+    distance: '15%',
+    origin: 'right',
+    easing: 'ease'
+});
+
+useScrolling('#avatar', {
+    delay: 100,
+    duration: 900,
+    distance: '10%',
+    origin: 'top',
+    easing: 'ease'
+});
+// ENDS HERE
+
+// TITLE
+useScrolling('.text-section', {
+    delay: 200,
+    distance: '10%',
+    origin: 'top',
+    easing: 'ease'
+});
+
+useScrolling('.text-highlight', {
+    delay: 300,
+    distance: '10%',
+    origin: 'top',
+    easing: 'ease'
+});
+// ENDS HERE
+
+// SERVICE CARDS
+useScrolling('.card-group .card-service', {
+    delay: 300,
+    distance: '15%',
+    origin: 'bottom',
+    easing: 'ease',
+    interval: 100
+});
+// ENDS HERE
+
+// WORKS
+useScrolling('.placeholder', {
+    delay: 300,
+    distance: '15%',
+    origin: 'bottom',
+    easing: 'ease'
+});
+
+useScrolling('.work-title', {
+    delay: 300,
+    distance: '5%',
+    origin: 'left',
+    easing: 'ease-out',
+});
+
+useScrolling('.built-with-using', {
+    delay: 300,
+    distance: '5%',
+    origin: 'top',
+    easing: 'ease',
+});
+
+useScrolling('.work-description', {
+    delay: 300,
+    distance: '5%',
+    origin: 'right',
+    easing: 'ease',
+});
+
+useScrolling('.work-action a', {
+    delay: 300,
+    distance: '10%',
+    origin: 'bottom',
+    easing: 'ease',
+});
+
+useScrolling('.work-img-wrapper a img', {
+    delay: 300,
+    distance: '5%',
+    origin: 'bottom',
+    easing: 'ease',
+});
+// ENDS HERE
+
+// SKILLS
+useScrolling('.skill-gallery img', {
+    delay: 300,
+    distance: '5%',
+    origin: 'top',
+    easing: 'ease',
+    interval: 50
+});
+// ENDS HERE
+
+// ABOUT ME
+useScrolling('.a-bit-about-me', {
+    delay: 300,
+    distance: '5%',
+    origin: 'right',
+    easing: 'ease'
+});
+
+useScrolling('.btn-download-resume', {
+    delay: 300,
+    distance: '15%',
+    origin: 'bottom',
+    easing: 'ease'
+});
+
+useScrolling('.img-drop-shadow', {
+    delay: 300,
+    distance: '25%',
+    origin: 'right',
+    easing: 'ease'
+});
+
+// useScrolling('#btn-download-resume', {
+//     delay: 500,
+//     distance: '10%',
+//     origin: 'left',
+//     easing: 'ease'
+// });
+
+useScrolling('#my-avatar', {
+    delay: 500,
+    distance: '10%',
+    origin: 'right',
+    easing: 'ease'
+});
+// ENDS HERE
